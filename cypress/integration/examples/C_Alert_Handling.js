79c622edcdae15cad5751f63a75319b8c98c228a
shubham <srkale856@gmail.com>
2025-07-25 08:03:15 +0530

    alert handling 2

--- cypress/integration/examples/C_Alert_Handling.js
+++ cypress/integration/examples/C_Alert_Handling.js
@@ -11,17 +11,21 @@ describe("This is my third test suit", function()
 
     cy.get("#confirmbtn").click(); // you just need to find the alert button and click on it cypress will itself handle it.
 
-    //      Getting text of alert popup
 
-    cy.on("window:alert", (str)=>
+
+/*    Getting text of alert popup -> to get the text present on alert pop up we need to use 'on' method 
+                                     which accepts the 'browser events and function to be executed after
+                                     event as parameter' 
+*/      
+   cy.on("window:alert", (str)=>
     {
         expect(str).to.equal("Hello , share this practice page and share your knowledge");
+        // above it the assertion for string in mocha framework
     })
 
     cy.on("window:confirm",(str)=>
     {
         expect(str).to.equal("Hello , Are you sure you want to confirm?");
     })
-
 })
 })
