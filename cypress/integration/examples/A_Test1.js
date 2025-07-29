d22a6682284e7bdaa351351a05b3cc17017870b9
shu8790 <109097952+shu8790@users.noreply.github.com>
2025-07-29 13:33:21 +0530

    Update A_Test1.js
    Added code explanation

--- cypress/integration/examples/A_Test1.js
+++ cypress/integration/examples/A_Test1.js
@@ -5,26 +5,38 @@ describe("This is my first suite", function()
     it("this is my first test case", function()
 {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+// visit() method is used to open an url
 
 // write a code to enter ca into input box and validate number of item in list is 4 only
 
-    cy.get(".search-keyword").type("ca");
+    cy.get(".search-keyword").type("ca"); // get method is used to get the elment present on web page
 
     cy.wait(5000);
 
-    cy.get(".product:visible").should("have.length",4);
+    cy.get(".product:visible").should("have.length",4); 
+// should() method is used to perform assertion in this case if u want to validate the length of elements collection,
+// you nedd to use should("have.length",4)
 
 // write the code to validate tha there are only 4 products but without visible
 cy.wait(5000);
 
-// alias method
+// alias method - as("")this method is used store web element locator as variable 
+// usage : 1. You want to reuse the same element multiple times in your test
+//          2. You want to make your code cleaner and more readable
 cy.get(".products").as("productsLocator")
 
 cy.get(".products").find(".product").should("have.length",4);
+// find() : The .find(selector) command is used to search for descendant elements
+// (i.e., children, grandchildren, etc.) within a previously selected element.
+// usage : Search only within .products, not the whole DOM
+
 
 // write code to click on add to cart button of second product
 cy.get(".products").find(".product").eq(1).contains("ADD TO CART").click();
-
+// eq(index): picks the element at the given index from a group of matched elements.
+//  usage: it is used when we want to select specific element, at code level not DOM level
+// contains(text) : contains("text") finds a element that has the given text
+//  usage : When you need to locate a button or label by visible text
 
 // click on the casshew product add to cart button
 cy.get("@productsLocator").find(".product").each(($el, index, $list)=> {
